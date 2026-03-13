395f997c336b54117ab6b90193b1d9ef562b6343
Santiago urrego <santiagourregolaya@gmail.com>
2026-03-12 19:41:14 -0500

    feat: Experience configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/layout/Navbar";
-import { Footer } from "./components/layout/Footer";
-import { Hero } from "./components/sections/Hero";
-import { Skills } from "./components/sections/Skills";
-import { Projects } from "./components/sections/Projects";
-import { Experience } from "./components/sections/Experience";
-import { About } from "./components/sections/About";
-import { Services } from "./components/sections/Services";
-import { ServicesPage } from "./pages/ServicesPage";
-import { ScrollToTop } from "./components/ui/ScrollToTop";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/layout/Navbar';
+import { Footer } from './components/layout/Footer';
+import { Hero } from './components/sections/Hero';
+import { Skills } from './components/sections/Skills';
+import { Projects } from './components/sections/Projects';
+import { Experience } from './components/sections/Experience';
+import { About } from './components/sections/About';
+import { Services } from './components/sections/Services';
+import { ServicesPage } from './pages/ServicesPage';
+import { ScrollToTop } from './components/ui/ScrollToTop';
 
 function PortfolioHome() {
   return (
